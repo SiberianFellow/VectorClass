8c3e4ea4e14cdce826d9e3bb0d512532385a93c8
Andrei <siberian.fellow@gmail.com>
2021-11-19 16:58:52 +0700

    Add project files.

---
+++ VectorClass/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace VectorClass 
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                Vector a = new Vector(1, 1, 0);
+                Vector b = new Vector(0, 1, 1);
+                Console.WriteLine("Первый вектор: ");
+                Vector.ShowVector(a);
+                Console.WriteLine("Второй вектор: ");
+                Vector.ShowVector(b);
+                Console.WriteLine("Length a: " + Vector.FindLength(a));
+                Console.WriteLine("Length b: " + Vector.FindLength(b));
+                Console.WriteLine("Scalar product: " + Vector.FindScalarProduct(a, b));
+                Vector crossProduct = Vector.FindCrossProduct(a, b);
+                Console.Write("Cross product: ");
+                Vector.ShowVector(crossProduct);
+                Console.WriteLine("Angle (measured in degrees) : " + Vector.FindAngle(a, b));
+                Vector summaryVector = Vector.FindSum(a, b);
+                Console.Write("Summary vector: ");
+                Vector.ShowVector(summaryVector);
+                Vector differenceVector = Vector.FindDifference(a, b);
+                Console.Write("Difference vector: ");
+                Vector.ShowVector(differenceVector);
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ VectorClass/Vector.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VectorClass
+{
+    internal class Vector
+    {
+        private double x;
+        private double y;
+        private double z;
+        public Vector(double x, double y, double z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        public static void ShowVector(Vector vec)
+        {
+            Console.WriteLine($"x = {vec.x}, y = {vec.y}, z = {vec.z}");
+        }
+        public static double FindLength(Vector vec)
+        {
+            return Math.Sqrt(vec.x * vec.x + vec.y * vec.y + vec.z * vec.z);
+        }
+        public static double FindScalarProduct(Vector vec1, Vector vec2)
+        {
+            return (vec1.x * vec2.x + vec1.y * vec2.y + vec1.z * vec2.z);
+        }
+        public static Vector FindCrossProduct(Vector vec1, Vector vec2)
+        {
+            return new Vector(vec1.y * vec2.z - vec1.z * vec2.y, vec1.z * vec2.x - vec1.x * vec2.z, vec1.x * vec2.y - vec1.y * vec2.x);
+        }
+        public static double FindAngle(Vector vec1, Vector vec2)
+        {
+            return Math.Round(Math.Acos(FindScalarProduct(vec1, vec2)/(FindLength(vec1)*FindLength(vec2)))*180/Math.PI, 2);
+        }
+        public static Vector FindSum(Vector vec1, Vector vec2)
+        {
+            return new Vector(vec1.x + vec2.x, vec1.y + vec2.y, vec1.z + vec2.z);
+        }
+        public static Vector FindDifference(Vector vec1, Vector vec2)
+        {
+            return new Vector(vec1.x - vec2.x, vec1.y - vec2.y, vec1.z - vec2.z);
+        }
+    }
+}
